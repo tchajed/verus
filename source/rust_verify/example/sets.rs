84dba43e81fe41e711ed595329f56460b4f908a9
Tej Chajed <tchajed@vmware.com>
2022-08-11 15:27:14 -0500

    Finish proving get_new_nat

--- source/rust_verify/example/sets.rs
+++ source/rust_verify/example/sets.rs
@@ -7,13 +7,13 @@ use pervasive::*;
 use pervasive::set::*;
 
 verus! {
-  pub open spec fn set_fold<T, R, F: Fn(R, T) -> R>(s: Set<T>, f: F, acc: R, count: nat) -> R {
+  pub open spec fn set_fold<T, R, F: Fn(T, R) -> R>(s: Set<T>, f: F, zero: R, count: nat) -> R {
       decreases(count);
       if count == 0 {
-          acc
+          zero
       } else {
           let x = s.choose();
-          set_fold(s.remove(x), f, f(acc, x), (count-1) as nat)
+          f(x, set_fold(s.remove(x), f, zero, (count-1) as nat))
       }
   }
 
@@ -23,32 +23,49 @@ verus! {
 
   pub open spec fn set_max(s: Set<nat>) -> (m: nat)
   {
-    set_fold(s, |n, m| max(n, m), 0, s.len())
+    set_fold(s, |x, y| max(x, y), 0, s.len())
+  }
+
+  pub proof fn set_max_fold_ok(s: Set<nat>, count: nat)
+    requires
+      s.finite(),
+      count == s.len(),
+    ensures forall |n: nat| s.contains(n) ==> set_fold(s, |x, y| max(x, y), 0, count) >= n
+  {
+    decreases(count);
+    if s.len() == 0 {
+      assume(s === Set::empty());
+    } else {
+      let x = s.choose();
+      set_max_fold_ok(s.remove(x), (count-1) as nat);
+      assert_forall_by(|n: nat| {
+        requires(s.contains(n));
+        ensures(set_fold(s, |x, y| max(x, y), 0, count) >= n);
+        if x != n {
+          assert(s.remove(x).contains(n));
+        } else {
+        }
+      })
+    }
   }
 
   pub proof fn set_max_ok(s: Set<nat>)
     requires s.finite()
     ensures forall |n: nat| s.contains(n) ==> set_max(s) >= n
   {
-    decreases(s.len());
-    if s.len() == 0 {
-      assume(s === Set::empty());
-      assert(forall |n: nat| !s.contains(n));
-    } else {
-      let x = s.choose();
-      set_max_ok(s.remove(x));
-      assert_forall_by(|n: nat| {
-        requires(s.contains(n));
-        ensures(set_max(s) >= n);
-        if x != n {
-          assert(s.remove(x).contains(n));
-          assert(set_max(s.remove(x)) >= n);
-          assert(set_max(s) === max(set_max(s.remove(x)), x));
-        } else {
-          assume(false);
-        }
-      })
-    }
+    set_max_fold_ok(s, s.len());
+  }
+
+  pub closed spec fn get_new_nat(s: Set<nat>) -> nat
+  {
+    set_max(s) + 1
+  }
+
+  pub proof fn get_new_nat_not_in(s: Set<nat>)
+    requires s.finite()
+    ensures !s.contains(get_new_nat(s))
+  {
+    set_max_ok(s);
   }
 
   fn main() {}
