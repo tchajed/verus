af997612337b4ad3439a5fa6703b352e6058fa42
Tej Chajed <tchajed@vmware.com>
2022-08-15 11:18:55 -0500

    Checkpoint some work on sets
    Stuck on an erasure bug

--- source/rust_verify/example/sets.rs
+++ source/rust_verify/example/sets.rs
@@ -5,7 +5,7 @@ use builtin::*;
 mod pervasive;
 use pervasive::*;
 use pervasive::set::*;
-use pervasive::set_lib::lemma_len0_is_empty;
+use pervasive::set_lib::{lemma_len0_is_empty, lemma_len_subset};
 
 verus! {
   impl<A> Set<A> {
@@ -63,6 +63,74 @@ verus! {
   {
     set_max_ok(s);
   }
+
+  pub open spec fn map_fold<A, B, F: Fn(A) -> B>(s: Set<A>, f: F) -> Set<B> {
+    s.right_fold::<Set<B>, _>(|x, ss| ss.insert(f(x)), Set::empty())
+  }
+
+  pub proof fn lemma_map_as_fold<A, B, F: Fn(A) -> B>(s: Set<A>, f: F)
+    requires
+      s.finite(),
+    ensures
+      s.map(f) === map_fold(s, f),
+    decreases s.len(),
+  {
+    if s.len() === 0 {
+      lemma_len0_is_empty::<A>(s);
+    } else {
+      let a = s.choose();
+      lemma_map_as_fold(s.remove(a), f);
+
+      // assert(
+      //   map_fold(s.remove(a), f) ===
+      //   s.remove(a).right_fold(|ss, x| ss.insert(f(x)), Set::empty()).insert(f(a))
+      // );
+      assume(false);
+    }
+  }
+
+  /*
+  pub proof fn lemma_len_map<A, B, F: Fn(A) -> B>(s: Set<A>, f: F)
+      requires
+          s.finite(),
+      ensures
+          s.map(f).len() <= s.len(),
+      decreases
+          s.len(),
+  {
+      if s.len() == 0 {
+        assert(s.len() == 0);
+        lemma_len0_is_empty::<A>(s);
+        assert(s.map(f).ext_equal(Set::empty()));
+        return;
+      }
+
+      let a = s.choose();
+      lemma_len_map::<A, B, F>(s.remove(a), f);
+      assert(s.remove(a).map(f).len() <= s.len() - 1);
+      if exists|a2: A| s.remove(a).contains(a2) && f(a2) === f(a) {
+        let a2 = choose |a2: A| s.remove(a).contains(a2) && f(a2) === f(a);
+        assert_forall_by(|b|
+        {
+          requires(#[auto_trigger] s.map(f).contains(b));
+          ensures(#[auto_trigger] s.remove(a).map(f).contains(b));
+          let b_a = choose |b_a:A| s.contains(b_a) && b === f(b_a);
+          if b_a === a {
+            assert(s.remove(a).contains(a2));
+            assert(f(b_a) === f(a2));
+            assert(s.remove(a).map(f).contains(f(b_a)));
+          } else {
+            assert(s.remove(a).contains(b_a));
+            assert(s.remove(a).map(f).contains(f(b_a)));
+          }
+        });
+        lemma_len_subset::<B>(s.map(f), s.remove(a).map(f));
+        // assert_sets_equal!(s.remove(a).map(f), s.map(f));
+      } else {
+        assert_sets_equal!(s.remove(a).map(f), s.map(f).remove(f(a)));
+      }
+  }
+  */
 
   fn main() {}
 }
