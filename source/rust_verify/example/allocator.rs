1fcf8b99bc625905f7f6bd1ff829fbfabc2b817f
Tej Chajed <tchajed@vmware.com>
2022-08-29 13:33:31 -0700

    Track a pointer for bump allocation

--- source/rust_verify/example/allocator.rs
+++ source/rust_verify/example/allocator.rs
@@ -18,12 +18,16 @@ verus! {
   struct Allocator<V> {
     block: Vec<PPtr<V>>,
     perms: Tracked<Map<nat, PermissionOpt<V>>>,
+    // the next index into block to give out
+    next: usize,
   }
 
   impl<V> Allocator<V> {
     closed spec fn wf(self) -> bool {
-      &&& self.perms@.dom() === Set::new(|n: nat| n < self.block.len())
+      &&& self.perms@.dom() === Set::new(|n: nat| self.next <= n < self.block.len())
       &&& self.perms@.dom().finite()
+      &&& (forall |i: int| #[auto_trigger] 0 <= i < self.block.len() ==>
+            self.block@[i].id() == self.perms@[i as nat]@.pptr && self.block@[i].id() != 0)
     }
 
     fn new(size: usize) -> (v:Self)
@@ -33,30 +37,32 @@ verus! {
       let mut perms = tracked(Map::tracked_empty());
       proof {
         assert(perms@.dom() === Set::empty());
-        assert_sets_equal!(Set::new(|n: nat| n < 0), Set::empty());
+        assert_sets_equal!(Set::new(|n: nat| 0 <= n && n < 0), Set::empty());
       }
       let mut i: usize = 0;
       while i < size
       {
         invariant([
           block.len() == i,
-          perms@.dom() === Set::new(|n: nat| n < i),
-          Allocator { block, perms }.wf(),
+          Allocator { block, perms, next: 0 }.wf(),
         ]);
         let (p, perm) = PPtr::empty();
         block.push(p);
 
         proof {
-          // what a mess! can't even let-bind perms.borrow_mut(), results in an
-          // error
+          // what a mess! everything has to be marked tracked
+          let tracked perm = (tracked perm).get();
+          (tracked &perm).is_nonnull();
           (tracked perms.borrow_mut())
-            .tracked_insert(i as nat, (tracked perm).get());
-          assert_sets_equal!(perms.view().dom(), Set::new(|n: nat| n < i+1 as nat));
+            .tracked_insert(i as nat, tracked perm);
+          assert_sets_equal!(
+            perms.view().dom(),
+            Set::new(|n: nat| 0 <= n && n < i+1 as nat));
         }
 
         i = i + 1;
       }
-      Allocator { block, perms }
+      Allocator { block, perms, next: 0 }
     }
   }
 
