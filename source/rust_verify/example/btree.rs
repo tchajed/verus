cd5a01283e607729ef63915cb228b64197b0d286
Tej Chajed <tchajed@vmware.com>
2022-08-31 09:45:40 -0700

    Try to implement some concatenation stuff

--- source/rust_verify/example/btree.rs
+++ source/rust_verify/example/btree.rs
@@ -60,13 +60,105 @@ mod enc {
 }
 
 verus! {
+  // NOTE: associated functions are not supported
+  /*
+  trait Parser where Self: std::marker::Sized {
+    fn parse(data: Seq<u8>) -> Option<Self> {
+      no_method_body()
+    }
+  }
+  */
+
+  // NOTE: fn() types are not directly supported
+  /*
+  type Parser<T> = fn(Seq<u8>) -> Option<T>;
+  */
+
+  // NOTE: trait bounds on Self are not supported
+  /*
+  trait Parser<T> where Self: Fn(Seq<u8>) -> Option<T> {}
+  */
+
+  pub open spec fn concat<T>(s: Seq<Seq<T>>) -> Seq<T> {
+    decreases(s.len());
+    if s.len() == 0 {
+      Seq::empty()
+    } else {
+      s[0].add(concat(s.subrange(1, s.len())))
+    }
+  }
+
+  proof fn lemma_concat_uniform_length<T>(s: Seq<Seq<T>>, k: int)
+    requires forall |i: int| #[auto_trigger] 0 <= i < s.len() ==> s[i].len() == k
+    ensures concat(s).len() == k * s.len()
+  {
+    decreases(s.len());
+    if s.len() == 0 {}
+    else {
+      lemma_concat_uniform_length::<T>(s.subrange(1, s.len()), k);
+      assert(s[0].len() == k);
+      assert(concat(s).len() == k + k * (s.len() - 1));
+    }
+  }
+
+  proof fn lemma_concat_uniform_index<T>(s: Seq<Seq<T>>, k: int)
+    requires 0 <= k,
+          forall |i: int| #[auto_trigger] 0 <= i < s.len() ==> s[i].len() == k,
+    ensures concat(s).len() == k * s.len(),
+            forall |i: int, j: int| #[auto_trigger] 0 <= i < s.len() && 0 <= j < k ==> concat(s)[k * i + j] === s[i][j],
+  {
+    decreases(s.len());
+    lemma_concat_uniform_length::<T>(s, k);
+    if s.len() == 0 {}
+    else {
+      assert_forall_by(|i: int, j: int| {
+        requires(0 <= i < s.len() && 0 <= j < k);
+        ensures(concat(s)[k * i + j] === s[i][j]);
+        if i == 0 {
+        } else {
+          assert(i <= s.len() - 1);
+          // this non-linear arith is too hard
+          assume(k * i <= k * (s.len() - 1));
+          assert(k * i <= k * s.len() - k);
+          assert(k * i + j < k * s.len());
+          assert(k * i + j < concat(s).len());
+          assert(k * i + j === k + k * (i - 1) + j);
+          assert(s[0].len() == k);
+          assert(concat(s)[k * i + j] === concat(s)[k + k * (i - 1) + j]);
+          assert(concat(s)[k + k * (i-1) + j] === concat(s.subrange(1, s.len()))[k * (i - 1) + j]);
+          lemma_concat_uniform_index::<T>(s.subrange(1, s.len()), k);
+          assert(concat(s.subrange(1, s.len()))[k * (i - 1) + j] === s.subrange(1, s.len())[i-1][j]);
+        }
+      });
+    }
+  }
+
   mod model {
     use super::pervasive;
     use pervasive::{*, seq::*};
+    use super::enc;
+    use builtin::SpecOrd;
+    use super::concat;
 
     pub struct Hdr {
       pub next_entry: u16,
       pub offsets: Seq<u16>,
+    }
+
+    impl Hdr {
+      spec fn valid(self) -> bool {
+        // length needs to fit in a u16
+        self.offsets.len() < 0x1_0000
+      }
+
+      spec fn encode(self) -> Seq<u8>
+      {
+        enc::u16_le_bytes(self.next_entry).add(
+          enc::u16_le_bytes(self.offsets.len() as u16)
+        ).add(
+          concat(self.offsets.map(|_i, x| enc::u16_le_bytes(x)))
+        )
+      }
     }
 
     pub struct LeafEntry {
